969e7f4c10aea3dd72b1b65b215fd80c958990f4
sarthakbhimgade <101983259+sarthakbhimgade@users.noreply.github.com>
2023-02-07 19:54:13 +0530

    Add files via upload

---
+++ main/java/com/sarthak/wsafety/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.sarthak.wsafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class s4wSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new CountDownTimer(1000,500){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                SplashScreen.this.finish();
+            }
+        }.start();
+
+    }
+}
